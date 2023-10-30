baa9c897ceb744c5e37656ef1984b22a8f95ce49
ahmedSam22 <tahmsamir288@tutamail.com>
2023-10-30 16:04:26 +0200

    first

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,26 +1,63 @@
 import { Component, signal } from '@angular/core';
+import { GalleryModule, Gallery, GalleryRef, ImageItem, GalleryItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-content',
   standalone : true,
+  imports: [GalleryModule],
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent {
+  images: GalleryItem[] = []
 
-  box:any;
-  rect = signal(1000)
-  ngOnInit(){
-    this.hh() 
+  constructor(private gallery: Gallery){
+
   }
-  hh(){
-    this.box = document.getElementById('6');
-    let rect = this.box.getBoundingClientRect();
- 
-    if(rect.top < 80){
-      alert('here')
-    }
-     console.log(rect , '555');
+
+  //  images: GalleryItem = [
+  //   {
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },{
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },{
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },{
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },{
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },{
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },{
+  //     src: '../../../assets/images/demo.jpg',
+  //     thumbnail: '../../../assets/images/demo.jpg',
+  //     text: 'Sky & Clouds'
+  //   },
+  //   // ...
+  // ];
+  ngOnInit() {
+    this.images = [
+      new ImageItem({ src: '../../../assets/images/demo.jpg', thumb: 'hhh' }),
+      new ImageItem({ src: '../../../assets/images/demo.jpg', thumb: '../../../assets/images/demo.jpg' }),
+      new ImageItem({ src: '../../../assets/images/demo.jpg', thumb: '../../../assets/images/demo.jpg' }),
+      new ImageItem({ src: '../../../assets/images/demo.jpg', thumb: '../../../assets/images/demo.jpg' }),
+      new ImageItem({ src: '../../../assets/images/demo.jpg', thumb: '../../../assets/images/demo.jpg' }),
+      new ImageItem({ src: '../../../assets/images/demo.jpg', thumb: '../../../assets/images/demo.jpg' }),
+      // ... more items
+    ];
+
   }
-  
+
 }
