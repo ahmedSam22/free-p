24d0ad501a90515697281f427a8f69a6b79b8a67
ahmedSam22 <tahmsamir288@tutamail.com>
2023-11-01 05:30:45 +0200

    first

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,11 +1,9 @@
-import { OwlOptions } from './../../../../node_modules/ngx-owl-carousel-o/lib/models/owl-options.model.d';
-import { Component, signal } from '@angular/core';
-import { CarouselModule } from 'ngx-owl-carousel-o';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-content',
   standalone : true,
-  imports : [CarouselModule],
+  imports : [],
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
@@ -14,29 +12,5 @@ export class ContentComponent {
  
   ngOnInit(){
   }
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: true,
-    navSpeed: 700,
-    navText: ['aa', 'vv'],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 1
-      },
-      740: {
-        items: 1
-      },
-      940: {
-        items: 1
-      }
-    },
-    nav: true
-  }
-  
+
 }
